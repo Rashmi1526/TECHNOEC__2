f4708b2a23d2b3165c9e85872c5cf59692757527
Utkarsh Goel <66838142+utkarshgoel10@users.noreply.github.com>
2022-03-26 23:40:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Homepage from './components/homepage/Homepage';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Homepage /> */}
+      <Login />
+      {/* <Register /> */}
+      </div>
+  );
+}
+
+export default App;
