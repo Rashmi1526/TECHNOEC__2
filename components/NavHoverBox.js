46151b0458e99671b6ed7b9d314cc900ce1e039f
Rohit Choudhary <71605903+Rohitchaudhary13@users.noreply.github.com>
2022-03-28 00:47:58 +0530

    Update NavHoverBox.js

--- components/NavHoverBox.js
+++ components/NavHoverBox.js
@@ -22,7 +22,6 @@ export default function NavHoverBox({ title, icon, description }) {
             <Flex
                 h={200}
                 w={200}
-                w="100%"
                 flexDir="column"
                 alignItems="center"
                 justify="center"
